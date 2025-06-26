885ee8b5867e421c1f066c92f6a2a094f0cf30e8
Rober Colmenares <Roberjosecolmenares@gmail.com>
2025-06-25 20:52:27 -0400

    Actualiza 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,17 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Principal from './Componets/Principal'; // Ajusta la ruta según tu estructura
-import DetallePizza from './Componets/DetallePizza';
-import Barra from './Componets/Barra'; // Ajusta la ruta según tu estructura
-import Cart from './Views/Cart';
-import './assets/App.css'
-import Header from './Componets/Header';
-import Footer from './Componets/Footer';
+import Bienvenida from './Componets/Bienvenida'; // Ajusta la ruta
 
 function App() {
   return (
-   <>
-    <Barra></Barra>
-    <Header></Header>
-    <Routes>
-    <Route path="/" element={<Principal />} />
-    <Route path="/home" element={<Principal />} />
-      <Route path="/pizza/:id" element={<DetallePizza />} />
-      <Route path="/cart" element={<Cart />} />
-    </Routes>
-    <Footer></Footer>
-
-   </>
+    <>
+      <Barra />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Bienvenida />} />
+        <Route path="/home" element={<Principal />} />
+        <Route path="/pizza/:id" element={<DetallePizza />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
-
-export default App;
